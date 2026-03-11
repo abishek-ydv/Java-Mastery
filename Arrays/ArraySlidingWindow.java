914e689875704f1d26ee2aacebbb38e551d568c5
Abhishek Yadav <heyabishek@outlook.com>
2026-03-11 22:56:00 +0530

    Enhance ArraySlidingWindow class with additional sliding window methods for first negative integer and distinct count

--- Arrays/ArraySlidingWindow.java
+++ Arrays/ArraySlidingWindow.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ArraySlidingWindow {
 
     public static int maxSubArraySumGFG(int[] arr, int k){
@@ -19,14 +24,46 @@ public class ArraySlidingWindow {
         int maxSum = 0;
         int windowSum = 0;
         for(int i = 0; i < arr.length; i++){
-            windowSum += arr[i];
-            if(i >= k) windowSum -= arr[i-k];
-            if(i >= k-1) maxSum = Math.max(maxSum, windowSum);
+            windowSum += arr[i]; //in this we are adding window sum so this will execute and will create our first windo sum which we get at when i = k;
+            if(i >= k) windowSum -= arr[i-k]; // so this will start removing element and will execute when  i == k or greater than k and then we get our current windowSum and i-k gives the first index of that window. 
+            if(i >= k-1) maxSum = Math.max(maxSum, windowSum); // k-1 is the first complete window.
         }
         return maxSum;
     }
 
+    public static List<Integer> firstNegIntGFG(int arr[], int k) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i <= arr.length-k; i++){
+            int firstNegative = 0;
+            for(int j = i; j < i + k; j++){
+                if(arr[j] < 0) {
+                    firstNegative = arr[j];
+                    break;
+                }
+            }
+            list.add(firstNegative);
+        }
+        return list;
+    }
+
+    public static ArrayList<Integer> countDistinctGFG(int arr[], int k) {
+        ArrayList<Integer> distinctCountArr = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < arr.length; i++){
+            map.put(arr[i],map.getOrDefault(arr[i], 0)+1);
+            if(i >= k){
+                map.put(arr[i-k], map.get(arr[i-k])-1);
+                if(map.get(arr[i-k]) == 0) map.remove(arr[i-k]);
+            }
+            if(i >= k-1){
+                distinctCountArr.add(map.size());
+            }
+        }
+        return distinctCountArr;
+    }
+
     public static void main(String[] args) {
         System.out.println(maxSubArraySumGFG(new int[] {8819, 674, 8816, 7705, 5699, 5383, 6177, 2113, 1992}, 1));
+        System.out.println(firstNegIntGFG(new int[] {-8, 2, 3, -6, 10}, 2));
     }
 }
